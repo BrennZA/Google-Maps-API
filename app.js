6c8441d4dd20f44bb96561a9af14cae698373c00
Brenn_ZA <bswanepoel4025@gmail.com>
2021-01-27 06:39:26 +0200

    move map center object to capitalInfo array in global scope

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
+const capitalInfo = [
+    Cairo = {
+        position: { lat: 30.0444, lng: 31.2357 },
+        title: "Cairo. Egypt",
+    }
+];
+
 function initMap() {
 
     const map = new google.maps.Map(document.getElementById("map"), {
-        center: { lat: 30.0444, lng: 31.2357 },
+        center: capitalInfo[0].position,
         zoom: 5,
     });
 }
