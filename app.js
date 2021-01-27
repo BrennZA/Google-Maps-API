af1fd0cf35e60406de66d5afb559b520a6e26b53
Brenn_ZA <bswanepoel4025@gmail.com>
2021-01-27 06:35:29 +0200

    add google maps script | test with new map

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-alert("test")
+function initMap() {
+
+    const map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: 30.0444, lng: 31.2357 },
+        zoom: 5,
+    });
+}
