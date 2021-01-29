3137ac578eb9676b1f88405ec04973266f2b8429
Brenn_ZA <bswanepoel4025@gmail.com>
2021-01-29 18:53:01 +0200

    create and add info window click listener to capital marker

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const capitalInfo = [
     Cairo = {
         position: { lat: 30.0444, lng: 31.2357 },
         title: "Cairo. Egypt",
+        description: "Cairo, Egypt’s sprawling capital, is set on the Nile River. At its heart is Tahrir Square and the vast Egyptian Museum, a trove of antiquities including royal mummies and gilded King Tutankhamun artifacts. Nearby, Giza is the site of the iconic pyramids and Great Sphinx, dating to the 26th century BC. In Gezira Island’s leafy Zamalek district, 187m Cairo Tower affords panoramic city views.",
+        image: "https://d3rr2gvhjw0wwy.cloudfront.net/uploads/mandators/49581/file-manager/egypt-cairo.jpg"
     }
 ];
 
@@ -51,10 +53,18 @@ function initMap() {
         zoom: 5,
     });
 
+    let infoWindow = new google.maps.InfoWindow({});
+
     const capitalMarker = new google.maps.Marker({
         position: capitalInfo[0].position,
         title: capitalInfo[0].title,
         map: map,
+    });
+    capitalMarker.addListener("click", () => {
+        infoWindow = new google.maps.InfoWindow({
+            content: addInfoContainer(capitalInfo),
+        });
+        infoWindow.open(map, capitalMarker);
     });
 
     // LABELS FOR LANDMARK MARKERS
@@ -73,4 +83,16 @@ function initMap() {
     new MarkerClusterer(map, landmarkMarkers, {
         imagePath: "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
     });
-}
+
+    // FUNCTIONS
+    const addInfoContainer = function (info) {
+        const infoContainer = document.createElement("div");
+        infoContainer.innerHTML = `
+            <h3>${info[0].title}</h3>
+            <img src=${info[0].image}>
+            <p>${info[0].description}</p>
+        `
+        infoContainer.classList.add("infoWindow");
+        return infoContainer;
+    }
+};
